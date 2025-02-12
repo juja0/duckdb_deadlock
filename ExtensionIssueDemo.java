f2818ae3d716a6c1b6ad8add5593daa1908499f4
Juja <juja05@gmail.com>
2025-02-12 11:15:18 +0530

    print glibc info

--- ExtensionIssueDemo.java
+++ ExtensionIssueDemo.java
@@ -1,12 +1,15 @@
 /// usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.duckdb:duckdb_jdbc:1.2.0
 
+import java.io.IOException;
 import java.sql.*;
 
 public class ExtensionIssueDemo
 {
-	public static void main(String... args) throws SQLException
+	public static void main(String... args) throws SQLException, IOException, InterruptedException
 	{
+		printGlibcInfo();
+
 		System.out.println("getting connection");
 
 		try (Connection connection = DriverManager.getConnection("jdbc:duckdb::memory:test"))
@@ -19,15 +22,29 @@ public class ExtensionIssueDemo
 
 			resultSet.next();
 
-			System.out.println(resultSet.getString(1));
+			System.out.println();
+			System.out.println("duckdb platform: " + resultSet.getString(1));
+			System.out.println();
 
 			resultSet.close();
 
 			statement.execute("install httpfs; load httpfs;");
-			
+
 			statement.close();
 		}
 
 		System.out.println("done executing query");
+	}
+
+	private static void printGlibcInfo() throws IOException, InterruptedException
+	{
+		System.out.println();
+
+		new ProcessBuilder("ldd", "--version")
+				.inheritIO()
+				.start()
+				.waitFor();
+		
+		System.out.println();
 	}
 }
